2a7018c80489887d5c71673eb9ba61b13225a10a
Ashley Umetsu <ashley.umetsu@verizon.com>
2022-03-10 11:27:21 -0600

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import Fade from "react-reveal";
+
+class About extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const name = this.props.data.name;
+    const profilepic = "../images/profilepic.jpeg";
+    const bio = this.props.data.bio;
+    const email = this.props.data.email;
+    const resumeDownload = this.props.data.resumedownload;
+
+    const networks = this.props.data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+
+    return (
+      <section id="about">
+        <Fade duration={1000}>
+          <div className="row">
+            <div className="three columns">
+              <img
+                className="profile-pic"
+                src={profilepic}
+                alt="Picture of Ashley smiling"
+              />
+            </div>
+            <div className="nine columns main-col">
+              <h2>About Me</h2>
+
+              <p>{bio}</p>
+              <div className="row">
+                <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="email">
+                    <span>{email}</span>
+                  </p>
+                    <a href="https://www.linkedin.com/in/ashleyumetsu/">
+                      <i className="fa fa-linkedin" style={{fontSize: '25px'}}></i>
+                    </a>
+                </div>
+                <div className="columns download">
+                  <p>
+                    <a href={resumeDownload} className="button">
+                      <i className="fa fa-download"></i>Download Resume
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default About;
